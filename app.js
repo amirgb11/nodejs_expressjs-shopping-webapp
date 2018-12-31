b842aae9520fafdb07f73fe48e090cebca1d714a
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2018-12-31 22:58:08 +0330

    3. Setup MongoDB - Mongoose

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('localhost:27017/shopping')
 
 // view engine setup
 app.engine('.hbs' , expressHbs({defaultLayout: 'layout' , extname: '.hbs'}));
