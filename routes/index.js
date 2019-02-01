0af34d4a64912b03387388d6426356bf605dfcb5
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2019-02-01 20:23:33 +0330

    6. User Model - SignUp View - CSRF Protection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');   // package for csrf protection
+var csrfProtection = csrf();
+
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,5 +19,14 @@ router.get('/', function(req, res, next) {
     res.render('shop/index', { title: 'Shopping Cart' , products : productChunks });
   })
 });
+
+
+router.get('/user/signup' , (req , res , next) => {
+  res.render('user/signup' , { csrfToken : req.csrfToken()})
+});
+
+router.post('/user/signup' , (req , res , next) => {
+   res.redirect('/');
+})
 
 module.exports = router;
